1c9027219389cbe14af7b709e5c8013aa9f68b35
Michael Hennecke <michael.hennecke@intel.com>
2022-05-19 11:49:49 +0200

    add Lenovo-Lenox3 config files
    Signed-off-by: Michael Hennecke <michael.hennecke@intel.com>

---
+++ io500/isc22/lenovo/daos_env.mlx.sh
@@ -0,0 +1,13 @@
+# DAOS 2.0 client-side settings for Mellanox InfiniBand:
+
+THISUSER=$(whoami)
+
+export D_LOG_MASK="WARN"
+export D_LOG_FLUSH="$D_LOG_MASK"
+export D_LOG_STDERR_IN_LOG=0 # stderr in application output
+export D_LOG_FILE="/tmp/daos.$THISUSER.log"
+
+export FI_UNIVERSE_SIZE=16383
+export FI_OFI_RXM_USE_SRX=1
+export FI_VERBS_PREFER_XRC=1
+
